7b8e7e1a589946127c292f4be8d858f30f1d542e
bosmanpa <bsmn.ptrck@gmail.com>
2019-12-18 10:50:02 -0600

    second checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BotCollection from './BotCollection'
 import YourBotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
 
 class BotsPage extends React.Component {
   state={
     bots:[],
-    botArmy:[]
+    botArmy:[],
+    showInfo: false,
+    infoBot:[]
   }
   
   componentDidMount(){
@@ -15,11 +18,17 @@ class BotsPage extends React.Component {
   }
   
   handleClick = (bot, inArmy) => {
-    inArmy ? this.removeFromArmy(bot) : this.addToArmy(bot)
+    inArmy ? this.removeFromArmy(bot) : this.showInfo(bot)
   }
 
+  showInfo = (bot) => {
+    this.setState({showInfo:true, infoBot:bot})
+  } 
+
   addToArmy = (bot) => {
-    this.state.botArmy.includes(bot) ? null : this.setState({botArmy:[...this.state.botArmy, bot]})
+    this.state.botArmy.includes(bot) ? 
+    alert("This bot is already in your army!") : 
+    this.setState({botArmy:[...this.state.botArmy, bot], showInfo:false, infoBot:[]})
   }
 
   removeFromArmy = (bot) => {
@@ -27,11 +36,33 @@ class BotsPage extends React.Component {
     this.setState({botArmy:newArmy})
   }
 
+  goBack = () => {
+    this.setState({showInfo:false, infoBot:[]})
+  }
+
+
+
+  showSwitch = () =>{
+    if (this.state.showInfo === false){
+       return this.renderCollection()
+    } else if (this.state.showInfo === true){
+      return this.renderInfo()
+    }
+  }
+
+  renderCollection = () => {
+    return <BotCollection bots={this.state.bots} handleClick={this.handleClick} />
+  }
+
+  renderInfo = () => {
+    return <BotSpecs bot={this.state.infoBot} addToArmy={this.addToArmy} goBack={this.goBack}/>
+  }
+
   render() {
     return (
       <div>
         <YourBotArmy botArmy={this.state.botArmy} handleClick={this.handleClick} />
-        <BotCollection bots={this.state.bots} handleClick={this.handleClick} />
+        {this.showSwitch()}
       </div>
     );
   }
