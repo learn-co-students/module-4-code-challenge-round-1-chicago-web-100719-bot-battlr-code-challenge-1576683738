d36c80116a6e2ebd35a84ec5ae542bd937249e63
benjewman <bennyadave@gmail.com>
2019-12-18 10:08:41 -0600

    add and render army

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,36 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super()
+    this.state = {
+      bots: [],
+      botArmy: []
+    }
+  }
 
+  handleCollectionClick = (bot) => {
+    let newArmyArray = this.state.botArmy.slice()
+    newArmyArray.push(bot)
+    this.setState({botArmy: newArmyArray})
+  }
+
+
+  componentDidMount() {
+    fetch(' https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(resp => resp.json())
+    .then(bots => this.setState({bots}))
+  }
+  
   render() {
     return (
       <div>
         {/* put your components here */}
+        <YourBotArmy bots={this.state.botArmy} />
+        <BotCollection handleClick={this.handleCollectionClick} bots={this.state.bots} />
       </div>
     );
   }
