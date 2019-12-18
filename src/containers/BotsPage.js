a23318564f6d11fbcb021653ff959d9a04a6c36c
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-12-18 09:51:54 -0600

    fetch bots on component load

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,6 +2,16 @@ import React from "react";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state= {
+    bots: []
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(resp => resp.json())
+    .then(bots => console.log(bots))
+    .catch(err => console.log(err.message))
+  }
 
   render() {
     return (
